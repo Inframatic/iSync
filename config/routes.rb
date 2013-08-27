bac0fafb16550c35c82a9f7a94aa6ef0fec2b1f8
inframatic <isteffy@hotmail.com>
2013-08-27 09:41:40 -0400

    devise error caused by not including devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 ISync::Application.routes.draw do
+  devise_for :users
+
   resources :posts
   root to: 'posts#index'
 
-  get "pages/new"
-  get "pages/create"
-  get "pages/update"
-  get "pages/edit"
-  get "pages/destroy"
-  get "pages/index"
-  get "pages/show"
 
 end
