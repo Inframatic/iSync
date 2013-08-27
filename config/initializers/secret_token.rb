d166d5271584452b62cf66f1f684098fd7a6f9df
inframatic <isteffy@hotmail.com>
2013-08-27 08:02:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ISync::Application.config.secret_key_base = '2586e64fc0b8550da1015c492b94d1e547c41cb22167252b20e069fe10b4a10a88997244199d370998fd49dc4bc7b459862405135c9cce570e6de7ecf88572d3'
