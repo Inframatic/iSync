446722d918cfb6dac276af7638fd903bede7528b
inframatic <isteffy@hotmail.com>
2013-08-27 11:49:40 -0400

    added show, followers and following to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: @user, owner_type: "User").page(params[:page]).per_page(20)
+  end
+
+  def followers
+  	@user = User.find(params[:id])
+  	@users = @user.followers
+  end
+
+  def following
+    @user = User.find(params[:id])
+  	@users = @user.followed_users
+  end
 end
